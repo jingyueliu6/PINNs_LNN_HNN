fffcae8fbd014ccf3e079c7193c30189f8647a13
jingyueliu6 <jingyueliu1206@gmail.com>
2023-08-24 12:12:59 +0200

    codes for the experiment

--- real_one_cable_test.py
+++ real_one_cable_test.py
@@ -6,14 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import haiku as hk
-import DeLaN_model_v3 as delan
+import DeLaN_model_v4 as delan
 from scipy.io import loadmat
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def load_data():
-    annots = loadmat(f"./data/resample_data/data_test3.mat")
+    annots = loadmat(f".data/dataset_test3.mat")
     q = annots["q"]
     dq = annots["dq"]
     tau = annots["input"]
@@ -34,10 +34,10 @@ def rk4_step(f, x, y, t, h):
 
 
 states_real, targets_real, u = load_data()
+
 n = states_real.shape[0]
 print(n)
-
-with open(f"./models/franka_real_delan.jax", "rb") as f:
+with open(f"./models/real_one_cable_5Hz.jax", "rb") as f:
     data = pickle.load(f)
 
 hyper = data["hyper"]
@@ -49,7 +49,6 @@ activations = {
     'sigmoid': jax.nn.sigmoid,
 }
 
-#def structured_lagrangian_fn(q, qd, n_dof, shape, activation, epsilon, shift)
 lagrangian_fn = hk.transform(partial(
     delan.structured_lagrangian_fn,
     n_dof=hyper['n_dof'],
@@ -67,62 +66,64 @@ dissipative_fn = hk.transform(partial(
     activation=activations[hyper['activation2']]
 ))
 
+#input_transform_matrix(q, n_dof, actuator_dof, shape, activation)
+input_mat_fn = hk.transform(partial(
+    delan.input_transform_matrix,
+    n_dof=hyper['n_dof'],
+    actuator_dof=hyper['actuator_dof'],
+    shape=(hyper['n_width_i'],) * hyper['n_depth_i'],
+    activation=activations[hyper['activation1']]
+))
+
 lagrangian = lagrangian_fn.apply
 dissipative_mat = dissipative_fn.apply
+input_mat = input_mat_fn.apply
 
-forward_model = jax.jit(delan.forward_model(params=params, key=None, lagrangian=lagrangian, dissipative_mat=dissipative_mat, n_dof=7))
-time_step = 2.0e-3
+forward_model = jax.jit(delan.forward_model(params=params, key=None, lagrangian=lagrangian, dissipative_mat=dissipative_mat,
+                                    input_mat=input_mat, n_dof=hyper['n_dof']))
+
+time_step = 0.2
+
 
 state = states_real[0]
 states =[]
-steps = int(n)
-
-print(steps)
-for i in range(steps):
+for i in range(n):
     states.append(state)
     state = partial(rk4_step, forward_model, t=0.0, h=time_step)(states_real[i], u[i])
-    #state = partial(rk4_step, forward_model, t=0.0, h=time_step)(states[i], u[i])
+    #state = partial(rk4_step, forward_model, t=0.0, h=time_step)(state, u[i])
+
+
 
 states = np.array(states)
-t = np.arange(0, 2.0e-3*n, 2.0e-3)
-t_p = np.arange(0, time_step*steps, time_step)
+t = np.arange(0, time_step*n, time_step)
 font1 = {'weight': 'normal',
-         'size': 11,
+         'size': 24,
          }
 plt.rcParams['font.family'] = ['serif']
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+np.savetxt("./data_2_figure/one_segment_real_fitting.csv", states_real, delimiter=",")
+np.savetxt("./data_2_figure/one_segment_pred_fitting.csv", states, delimiter=",")
 
-error = jnp.sqrt(jnp.sum((states_real[:, :7] - states[:, :7])**2))/7
-print(error)
 t = t[:n]
 print(n)
 print(t.shape)
 print(states.shape)
-
-
-plt.plot(t, states_real[:n, 0], 'g', alpha=0.4, linewidth=1, label=r'$j_1^{ref}$')
-plt.plot(t, states[:, 0], 'g', ls='--', linewidth=1, label=r'$j_1^{grey}$')
-
-plt.plot(t, states_real[:n, 1], 'r', alpha=0.4,  linewidth=1, label=r'$j_2^{ref}$')
-plt.plot(t, states[:, 1], 'r',  ls='--', linewidth=1, label=r'$j_2^{grey}$')
-
-plt.plot(t, states_real[:n, 2], 'c', alpha=0.4,  linewidth=1, label='$j_3^{ref}$')
-plt.plot(t, states[:, 2], 'c',  ls='--', linewidth=1, label='$j_3^{grey}$')
-
-plt.plot(t, states_real[:n, 3], 'k', alpha=0.4, linewidth=1, label=r'$j_4^{ref}$')
-plt.plot(t, states[:, 3], 'k', ls='--', linewidth=1, label=r'$j_4^{grey}$')
-
-plt.plot(t, states_real[:n, 4], 'm', alpha=0.4,  linewidth=1, label=r'$j_5^{ref}$')
-plt.plot(t, states[:, 4], 'm',  ls='--', linewidth=1, label=r'$j_5^{grey}$')
-
-plt.plot(t, states_real[:n, 5], 'y', alpha=0.4,  linewidth=1, label='$j_6^{ref}$')
-plt.plot(t, states[:, 5], 'y',  ls='--', linewidth=1, label='$j_6^{grey}$')
-
-plt.plot(t, states_real[:n, 6], 'purple', alpha=0.4, linewidth=1, label='$j_7^{ref}$')
-plt.plot(t, states[:, 6], 'purple', ls='--', linewidth=1, label='$j_7^{grey}$')
-plt.legend(fontsize=11, loc='upper right')
-plt.ylabel("angle", font1)
+plt.subplot(1, 1, 1)
+plt.plot(t, states_real[:n, 0], 'g', alpha=0.4, linewidth=3, label=r'$\alpha^{ref}$')
+plt.plot(t, states[:, 0], 'g', ls='--', linewidth=3, label=r'$\alpha^{grey}$')
+plt.plot(t, states_real[:n, 1], 'r', alpha=0.4,  linewidth=3, label=r'$\beta^{ref}$')
+plt.plot(t, states[:, 1], 'r',  ls='--', linewidth=3, label=r'$\beta^{grey}$')
+plt.plot(t, states_real[:n, 2], 'c', alpha=0.4,  linewidth=3, label='$\gamma^{ref}$')
+plt.plot(t, states[:, 2], 'c',  ls='--', linewidth=3, label='$\gamma^{grey}$')
+plt.legend(fontsize=24, loc='upper right')
+plt.ylabel("Orientation", font1)
 plt.xlabel('time[s]', font1)
-plt.yticks(size=11)
-plt.xticks(size=11)
+plt.yticks(size=17)
+plt.xticks(size=17)
 plt.show()
+
+
+accuracy_delan = np.sum((states - states_real)**2)/len(t)
+print("accuracy_delan: ", accuracy_delan)
+
+
